0b8aaea56c4286e0c007038d26a7388597693a92
Endres, Thomas <t.endres@mediosmanagement.de>
2016-09-26 15:19:52 +0200

    initial commit

---
+++ src/main/java/de/tom/endres/se/ha/WerkstattApplication.java
@@ -0,0 +1,57 @@
+package de.tom.endres.se.ha;
+
+import net.aksingh.owmjapis.CurrentWeather;
+import net.aksingh.owmjapis.DailyForecast;
+import net.aksingh.owmjapis.OpenWeatherMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class WerkstattApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(WerkstattApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerkstattApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner doSomething() {
+		return (args) -> {
+			// declaring object of "OpenWeatherMap" class
+			OpenWeatherMap owm = new OpenWeatherMap(OpenWeatherMap.Units.METRIC, OpenWeatherMap.Language.GERMAN,"<apikey>");
+
+			// getting current weather data for the "London" city
+			CurrentWeather cwd = owm.currentWeatherByCityName("Melchow");
+			// checking data retrieval was successful or not
+			if (cwd.isValid()) {
+
+				// checking if city name is available
+				if (cwd.hasCityName()) {
+					//printing city name from the retrieved data
+					log.info("City: " + cwd.getCityName());
+				}
+
+				// checking if max. temp. and min. temp. is available
+				if (cwd.getMainInstance().hasMaxTemperature() && cwd.getMainInstance().hasMinTemperature()) {
+					// printing the max./min. temperature
+					log.info("Temperature: " + cwd.getMainInstance().getMaxTemperature()
+							+ "/" + cwd.getMainInstance().getMinTemperature() + "\'C");
+				}
+
+				DailyForecast dfc = owm.dailyForecastByCityName("Melchow", (byte) 1);
+				// checking for rain
+				if (dfc.getForecastInstance(0).hasRain()) {
+					// printing the max./min. temperature
+					log.info("Rain: " + dfc.getForecastInstance(0).getRain());
+				}
+			}
+		};
+	}
+}
---
+++ src/test/java/de/tom/endres/se/ha/WerkstattApplicationTests.java
@@ -0,0 +1,16 @@
+package de.tom.endres.se.ha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WerkstattApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
